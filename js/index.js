5e2d6d4d141f7e5ee2951a8092581bc8c8487767
rjspyk5 <rjspyk5@gmail.com>
2024-02-28 16:52:22 +0600

    Find data from api function added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log("test");
+const searchBox = document.getElementById("search-box");
+const submitBtn = document.getElementById("submit");
+
+// find data
+const findData = async (searchIteam) => {
+  const promisedData = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchIteam}`
+  );
+  const parsedData = await promisedData.json();
+  const alldata = parsedData.data;
+  alldata.forEach((el) => {
+    console.log(el);
+  });
+};
+// Set data on website
+// Initial event listener function
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const searchIteam = searchBox.value;
+  findData(searchIteam);
+};
+submitBtn.addEventListener("click", handleSubmit);
