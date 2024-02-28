18c3d3e6de697710575e9aeccaac01129cf01453
rjspyk5 <rjspyk5@gmail.com>
2024-02-28 18:08:07 +0600

    load data form api

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const searchBox = document.getElementById("search-box");
 const submitBtn = document.getElementById("submit");
+const cardContainer = document.getElementById("card-container");
 
 // find data
 const findData = async (searchIteam) => {
@@ -8,11 +9,29 @@ const findData = async (searchIteam) => {
   );
   const parsedData = await promisedData.json();
   const alldata = parsedData.data;
+
   alldata.forEach((el) => {
-    console.log(el);
+    setDataOnCard(el);
   });
 };
+
 // Set data on website
+const setDataOnCard = (el) => {
+  const div = document.createElement("div");
+  div.className = "max-w-sm bg-white border border-gray-200 rounded-lg shadow";
+  div.innerHTML = `
+ <img class=${el?.image} alt="${el?.phone_name} img"/>
+ <div class="p-5 flex flex-col justify-center items-center">
+     <h5 class="mb-2 text-2xl font-bold text-center">${el?.phone_name}</h5>
+     
+     <a id=${el.slug} href="#"
+         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+         Show Details
+     </a>
+ </div>
+ `;
+  cardContainer.appendChild(div);
+};
 // Initial event listener function
 const handleSubmit = (e) => {
   e.preventDefault();
